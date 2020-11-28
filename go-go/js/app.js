3087c12f910cfeaefed378f6dec5e7201f5677d2
dmitriyolari <69803475+dmitriyolari@users.noreply.github.com>
2020-11-28 11:42:54 +0200

    Add files via upload

---
+++ go-go/js/app.js
@@ -0,0 +1,62 @@
+$(function () {
+
+
+  $(".slider-new-tel-three").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+    ]
+  });
+  $(".slider-new-tel-four").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+  $(document).ready(function(){
+    $('#myCarousel').carousel({
+      pause: true,
+      interval: false
+    });
+
+  });
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.slider-new-tel-three').slick('setPosition');
+  })
+
+});
+
+
